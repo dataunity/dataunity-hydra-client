5a182d7b27e95e2ff31503cbf978380e9c7c39d0
Kev K <kev@dataunity.org>
2014-07-04 19:59:06 +0100

    Add form functionality.

--- js/dataunity-hydraclient-0.1.0.js
+++ js/dataunity-hydraclient-0.1.0.js
@@ -9,7 +9,8 @@ angular.module('dataunity-hydra', [])
             supportedProperty: hydraBase + "supportedProperty",
             supportedOperation: hydraBase + "supportedOperation",
             property: hydraBase + "property",
-            method: hydraBase + "method"
+            method: hydraBase + "method",
+            expects: hydraBase + "expects"
         }
     })
 
@@ -62,22 +63,32 @@ angular.module('dataunity-hydra', [])
         }
     })
      
-    .controller('HydraCtrl', function($scope, $http, jsonldHelper, rdfsNs, hydraNs) {
+    .controller('HydraCtrl', function($scope, $sce, $http, jsonldHelper, rdfsNs, hydraNs) {
         // Dependencies
         var jsonLd = jsonld,
             jsonldPromises = jsonld.promises();
+
+        // Helper for HTML form action url
+        //$scope.trustSrc = function(src) {
+        //    return $sce.trustAsResourceUrl(src);
+        //};
 
         //var projectUrl = fbURL + $routeParams.projectId;
         $scope.currentData = null;
         $scope.apiDoc = null;
         $scope.supportedPropertiesLookup = {};
         $scope.hydraClassesLookup = {};
+        $scope.areLinksVisible = true;
+        $scope.isFormVisible = false;
         $scope.links = [];
         $scope.title = "";
 
-        var getAPIProperty = function (supportProp) {
-            return angular.isArray(supportProp[hydraNs.property]) ? supportProp[hydraNs.property][0] : supportProp[hydraNs.property];
-        };
+        $scope.formURL = "";
+        $scope.formMethod = "GET";
+        //$scope.formInputs = [];
+        $scope.hydraForm = {};
+        $scope.hydraForm.data = {};
+        $scope.hydraForm.data.fields = [];  
 
         $scope.indexAPIDoc = function () {
             // Produces indexes for the current API doc
@@ -103,7 +114,7 @@ angular.module('dataunity-hydra', [])
                 numSupportProps = typeof supportProps === "undefined" ? 0 : supportProps.length
                 for (propIndex = 0; propIndex < numSupportProps; propIndex++) {
                     supportProp = supportProps[propIndex];
-                    prop = getAPIProperty(supportProp);
+                    prop = getProperty(supportProp);
                     if (prop && prop["@id"]) {
                         propId = prop["@id"];
                         $scope.supportedPropertiesLookup[propId] = supportProp;
@@ -165,7 +176,7 @@ angular.module('dataunity-hydra', [])
                             $scope.indexAPIDoc();
 
                             // Load the page
-                            $scope.populatePage();
+                            $scope.showPage();
                         }, 
                         function (err) {
                             throw "There was an error expanding API Doc";
@@ -176,7 +187,7 @@ angular.module('dataunity-hydra', [])
                 });
         };
 
-        $scope.populatePage = function () {
+        $scope.showPage = function () {
             // Load page from current data
             var currentData = angular.isArray($scope.currentData) ? $scope.currentData[0] : $scope.currentData,
                 pageTypes = [],
@@ -184,6 +195,9 @@ angular.module('dataunity-hydra', [])
                 index = 0,
                 prop = null,
                 linkVal = "";
+            $scope.areLinksVisible = true;
+            $scope.isFormVisible = false;
+
             console.log("currentData");
             console.log(currentData);
 
@@ -206,6 +220,107 @@ angular.module('dataunity-hydra', [])
                 }
             });
 
+        };
+
+        // Form
+        $scope.showForm = function (url, prop, method) {
+            // Load page from current data
+            var currentData = angular.isArray($scope.currentData) ? $scope.currentData[0] : $scope.currentData,
+                //pageTypes = [],
+                payloadTypes = [],
+                formCls = null,
+                index = 0,
+                // Find the operation in the property
+                suppOp = getSupportedOperation(prop, method),
+                formSuppProps = [],
+                linkVal = "";
+
+            if (suppOp == null) {
+                throw "Can't build form, no supported operation in property with the method '" + method + "'";
+            }
+
+            $scope.areLinksVisible = false;
+            $scope.isFormVisible = true;
+            $scope.title = jsonldHelper.getLiteralValue(suppOp, rdfsNs.label);
+
+            console.log("form property");
+            console.log(prop);
+            console.log("form op");
+            console.log(suppOp);
+
+            // Lookup the hydra:Class which provides the form details
+            payloadTypes = jsonLd.getValues(suppOp, hydraNs.expects);
+            if (payloadTypes) {
+                for (index = 0; index < payloadTypes.length; index++) {
+                    //console.log("Before");
+                    //console.log(payloadTypes[index]);
+                    payloadTypes[index] = jsonldHelper.getIdValue(payloadTypes[index]);
+                    //console.log("After");
+                    //console.log(payloadTypes[index]);
+                }
+            }
+            
+
+            //console.log("payloadTypes");
+            //console.log(payloadTypes);
+
+            //pageTypes = jsonLd.getValues(currentData, "@type");
+            formCls = $scope.lookupHydraClass(payloadTypes);
+
+            console.log("form class");
+            console.log(formCls);
+
+            console.log("form url");
+            console.log(url);
+
+            formSuppProps = jsonLd.getValues(formCls, hydraNs.supportedProperty);
+            console.log("form items");
+            console.log(formSuppProps);
+
+            //$scope.formURL = url;
+            $scope.formURL = $sce.trustAsResourceUrl(url);
+            $scope.formMethod = method;
+
+            // Note: used following as basis for dynamic form elements:
+            // http://jsfiddle.net/langdonx/6H8Xx/2/
+            angular.forEach(formSuppProps, function (value, key) {
+                $scope.hydraForm.data.fields.push(value);
+            });
+        };
+
+        $scope.submitForm = function () {
+            var formData = {},
+                jsonData = {},
+                url = $scope.formURL,
+                method = $scope.formMethod,
+                requestHeaders = {"Content-Type": "application/json"};
+            //console.log("Form test");
+            //console.log($scope.hydraForm.data);
+            angular.forEach($scope.hydraForm.data.fields, function (suppProp, key) {
+                var fieldName = $scope.getSupportedPropertyPropertyId(suppProp)
+                    fieldValue = suppProp.data;
+                if (typeof fieldValue !== 'undefined') {
+                    formData[fieldName] = fieldValue;
+                }                    
+                //console.log(suppProp);
+            });
+            console.log("formData");
+            console.log(formData);
+
+            jsonData = JSON.stringify(formData);
+            $http({
+                method: method, 
+                url: url, 
+                data: jsonData, 
+                headers: requestHeaders})
+                .success(function(data, status, headers, config) {
+                    console.log("Got data back");
+                    console.log(data);
+                })
+                .error(function(data, status, headers, config) {
+                    console.log("Error");
+                    console.log(status);
+                });
         };
 
         $scope.lookupHydraClass = function (types) {
@@ -234,7 +349,7 @@ angular.module('dataunity-hydra', [])
             var supportProp = $scope.lookupSupportedProperty(suppPropIRI),
                 prop = null;
             if (supportProp != null) {
-                prop = getAPIProperty(supportProp);
+                prop = getProperty(supportProp);
             }
             return prop;
         };
@@ -242,37 +357,64 @@ angular.module('dataunity-hydra', [])
         // Helper functions for display
         $scope.rdfsNs = rdfsNs;
 
+        var getProperty = function (supportProp) {
+            return angular.isArray(supportProp[hydraNs.property]) ? supportProp[hydraNs.property][0] : supportProp[hydraNs.property];
+        };
+
+        $scope.getSupportedPropertyLabel = function (suppProp) {
+            var prop = getProperty(suppProp);
+            if (prop == null) {
+                return "Unknown";
+            }
+            return jsonldHelper.getLiteralValue(prop, rdfsNs.label)
+        };
+
+        $scope.getSupportedPropertyPropertyId = function (suppProp) {
+            // Gets the id of the property belonging to the SupportedProperty
+            var prop = getProperty(suppProp);
+            if (prop == null) {
+                throw "SupportedProperty doesn't have a property so unable to fetch id."
+            }
+            return jsonldHelper.getIdValue(prop, "@id")
+        };
+
         $scope.getLiteralValue = function (subject, predicate) {
             return jsonldHelper.getLiteralValue(subject, predicate);
-        }
+        };
 
-        var hasOperationMethod = function (prop, method) {
-            // Whether the hydra:property has the operation method
-            //var suppOps = jsonldHelper.getValue(prop, hydraNs.supportedOperation),
+        var getSupportedOperation = function (prop, method) {
             var suppOps = jsonLd.getValues(prop, hydraNs.supportedOperation),
                 suppOp = null,
+                tmpSuppOp = null,
                 tmpMethod = "",
                 index = 0;
             if (suppOps) {
                 for (index = 0; index < suppOps.length; index++) {
-                    suppOp = suppOps[index];
-                    tmpMethod = jsonldHelper.getLiteralValue(suppOp, hydraNs.method);
+                    tmpSuppOp = suppOps[index];
+                    tmpMethod = jsonldHelper.getLiteralValue(tmpSuppOp, hydraNs.method);
                     if (tmpMethod === method) {
-                        return true;
+                        suppOp = tmpSuppOp;
+                        break;
                     }
                 }
             }
-            return false;
-        }
+            return suppOp;
+        };
+
+        var hasOperationMethod = function (prop, method) {
+            // Whether the hydra:property has the operation method
+            //var suppOps = jsonldHelper.getValue(prop, hydraNs.supportedOperation),
+            var suppOp = getSupportedOperation(prop, method);
+            return suppOp ? true : false;
+        };
 
         $scope.hasGET = function (prop) {
             // Whether the hydra:property has a GET operation
             return hasOperationMethod(prop, "GET");
-        }
+        };
         $scope.hasPOST = function (prop) {
             // Whether the hydra:property has a POST operation
             return hasOperationMethod(prop, "POST");
-        }
-
+        };
     });
 
